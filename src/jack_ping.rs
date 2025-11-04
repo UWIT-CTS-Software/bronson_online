fd1b6f36a040eb8b37d4e1a41f3ad23986b79784
Alex Bryan <abryan9@uwyo.edu>
2025-11-04 16:57:50 -0700

    Oops x2.

--- src/jack_ping.rs
+++ src/jack_ping.rs
@@ -26,7 +26,7 @@ pub mod jp {
                 },
             Err(m) => {
                 println!("Error Pinging {}", hostname);
-                return Err(m);
+                return Err(m.to_string());
                 },
         }
 
