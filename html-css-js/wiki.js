3c9ca530bc42f1da940f3cfd7ababc103b1ca389
DJReflexive <lfermeli@uwyo.edu>
2026-03-30 14:24:24 -0600

    Ripped out unneeded code & created wiki directory

--- html-css-js/wiki.js
+++ html-css-js/wiki.js
@@ -1,4 +1,4 @@
-    /*
+/*
             _ _    _   _     
             (_) |  (_) (_)    
     __      ___| | ___   _ ___ 
@@ -32,8 +32,6 @@ async function setWiki() {
     document.title = "Wiki - Bronson";
     history.pushState("test", "Wiki", "/wiki");
     
-    
-    
     // remove currently active status mark tab has active.
     // Update active_tab_header
     // let active_tab_header = document.querySelector('.active_tab_header');
@@ -49,93 +47,31 @@ async function setWiki() {
     let main_container = document.createElement('div');
     main_container.classList.add('program_guts');
     
-    // Wiki editor container
-    let w_editor = document.createElement('div');
-    w_editor.classList.add("w_editor");
 
-    // Wiki input page
-    let w_input = document.createElement('div');
-    w_input.classList.add("w_input");
-    w_input.innerHTML = `
-        <textarea id="input" class="inputWindow" placeholder="# MMm markdown" onkeyup="updatePreview()"></textarea>`;
+    /* -------------------- Wiki Page -------------------- */
 
-    // Wiki preview/output Page
-    let w_preview = document.createElement('div');
-    w_preview.classList.add("w_preview");
-    w_preview.innerHTML = `
-        <p>
-            This is text
-        </p><br>`;
-
-    // Option Menu buttons
-    // [ Generate Files ] [ Clear Console ] [ Reset ]
-    let optionMenu = document.createElement("div");
-    optionMenu.classList.add('w_optionMenu');
-    optionMenu.innerHTML = `
-        <fieldset class='w_fieldset'>
-            <legend class='w_legend'>
-                Options: </legend>
-            <button id="run" onclick="wiki_button()"> 
-                Wiki Button </button>
-            <button id="reset" onclick="setWiki()"> 
-                Reset </button>
-        </fieldset>`;
-
-    // Option Menu buttons
-    // [ Generate Files ] [ Clear Console ] [ Reset ]
     let w_toc = document.createElement("div");
     w_toc.classList.add('w_toc');
-    // replace <ul> with something else
+
     let article_list_html = await getTocHTML();
     w_toc.innerHTML = article_list_html;
-    //     <fieldset class='w_fieldset'>
-    //         <legend class='w_legend'>
-    //             Articles: 
-    //         </legend>
-    //         <ul>
-    //             <li> Item 1 </li>
-    //             <li> Item 2 </li>
-    //             <li> Item tmp </li>
-    //             <li> Item 3 </li>
-    //         </ul>
-    //     </fieldset>`;
 
-    w_editor.appendChild(w_input);
-    w_editor.appendChild(w_preview);
-    w_editor.appendChild(optionMenu);
-    w_editor.appendChild(w_toc)
-    main_container.appendChild(w_editor);
-    // main_container.appendChild(bottomMenu);
+    main_container.appendChild(w_toc);
     progGuts.replaceWith(main_container);
-    return;
 }
 
 async function getTocHTML() {
-    let default_html = `
-        <fieldset class='w_fieldset'>
-            <legend class='w_legend'>
-                Articles: 
-            </legend>
-            <ul>
-                <li> Item 1 </li>
-                <li> Item 2 </li>
-                <li> Item tmp </li>
-                <li> Item 3 </li>
-            </ul>
-        </fieldset>`;
-
     let articles = await getW_BuildArticles();
     console.log(articles);
 
     let html = `
         <fieldset class='w_fieldset'>
             <legend class='w_legend'>
-                Articles:
+                Table of Contents:
             </legend>
-            <p> Test File </p>
-            <p> Test File 2 </p>
-            <p> ${articles[0]} </p>
-        </fieldset>`;
+            ${articles.map(article => `<p>${article}</p>`).join('')}
+        </fieldset>
+    `;
     
     return html;
 }
